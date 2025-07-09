dbe275f80aeeebd89d0eb4cbe9a92d37abbdefd7
Alma-yovell <alma.yovell@gmail.com>
2025-07-09 15:01:45 +0300

    תחלת חלק 0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 import IntroLomda from './GenericComponent/IntroLomda';
 import PartZero from './PartsComponent/PartZeroComponent/PartZero';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <div className="App">
-      <img src={`${process.env.PUBLIC_URL}/Assets/logos/logo.png`} alt="main-logo" className="main-logo" />
+      <img src={`${process.env.PUBLIC_URL}/Assets/logos/logo.png`} alt="main-logo" className="main-logo" onClick={() => navigate('/')} />
       <img src={`${process.env.PUBLIC_URL}/Assets/logos/iLogo.png`} alt="iLogo" className="i-logo" />
+
+      {/* <div className='info-part'>
+        <div className='info-overlay '>
+          <div className='info-text'>
+            מפתחות:   <br />
+            עלמה יובל  <br />
+            אביטל גמבורג
+            <br />
+            <br />
+            גרפיקאיות:  <br />
+            עלמה יובל <br />
+            אביטל גמבורג <br />
+            אגם
+            <br />
+            <br />
+            מומחה תוכן:  <br />
+            שליו אלפסי <br />
+            איריס
+            <br />
+            <br />
+            מנהלת פרוקייט:  <br />
+            תמר בוסטן
+          </div>
+        </div>
+      </div> */}
+
       <Routes>
         <Route path="/" element={<IntroLomda />} />
         <Route path="part-zero" element={<PartZero />} />
--- src/PartsComponent/PartZeroComponent/PartZero.jsx
+++ src/PartsComponent/PartZeroComponent/PartZero.jsx
@@ -3,10 +3,39 @@ import '../PartZeroComponent/PartZero.css'
 
 const PartZero = () => {
 
+    const [showTextDiv, setShowTextDiv] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowTextDiv(true);
+        }, 100);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div id="PartZero">
             <img src={`${process.env.PUBLIC_URL}/Assets/PartZeroImgs/Ambulance.png`} alt="Ambulance" className="ambulance-zero" />
-            <div className='divText-Ambulance'>
+            <div className={`divText-Ambulance ${showTextDiv ? 'fade-in' : ''}`}>
+                <div id='text-Ambulance1'>
+                    <h2>
+                        ברוכים הבאים ללומדת מכלול רפואה!
+                    </h2>
+                    <p>
+                        בלומדה זו תעבור הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר
+                    </p>
+                </div>
+                <div id="text-Ambulance2" style={{ display: "none" }}>
+                    <h2>
+                        בלומדה ילווה אותם תומר- קצין רפואה
+                    </h2>
+                    <p>
+                        בלומדה ילווה אותם תומר- קצין רפואה
+                        הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר הסבר
+                    </p>
+                </div>
+                <img src={`${process.env.PUBLIC_URL}/Assets/PartZeroImgs/Tomer.png`} alt="Tomer" className="Tomer-zero" style={{ display: "none" }} />
+
             </div>
         </div>
     );
